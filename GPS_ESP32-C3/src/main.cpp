b710ae506ab41cb8e106e0e26764cb9f000ffef1
guillermogonzalo365-dev <guillermogonzalo365@gmail.com>
2025-09-27 16:35:32 +0200

    inicializado UART del GPS

--- GPS_ESP32-C3/src/main.cpp
+++ GPS_ESP32-C3/src/main.cpp
@@ -1,15 +1,21 @@
+// IMPORTANTE: ESTE PROGRAMA SOLO FUNCIONA EN EL ESP32-C3
 #include <Arduino.h>
 #include <TinyGPSPlus.h>
 
-
-// IMPORTANTE: ESTE PROGRAMA SOLLO FUNCIONA EN EL ESP32-C3
+HardwareSerial GPS_SERIAL(0); // Usar UART0 para el GPS
+TinyGPSPlus gps;  
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  delay(3600); // Espera a que el monitor serie se conecte (especialmente útil para USB CDC)
+Serial.begin(115200);
+Serial.println("Inicio de programa");
 
+GPS_SERIAL.begin(9600, SERIAL_8N1, 20, 21); // RX en GPIO3, TX en GPIO1 (TX no se usa)
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 }
 
