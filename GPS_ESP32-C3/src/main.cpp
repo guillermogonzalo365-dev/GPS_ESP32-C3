e4ff3be5c334da73536ef5430fdf0f83685c2530
guillermogonzalo365-dev <guillermogonzalo365@gmail.com>
2025-09-28 18:48:33 +0200

    V 0.1

--- GPS_ESP32-C3/src/main.cpp
+++ GPS_ESP32-C3/src/main.cpp
@@ -1,6 +1,7 @@
 // IMPORTANTE: ESTE PROGRAMA SOLO FUNCIONA EN EL ESP32-C3
 #include <Arduino.h>
 #include <TinyGPSPlus.h>
+#include <SoftwareSerial.h> //si necesitamos más uart
 
 HardwareSerial GPS_SERIAL(0); // Usar UART0 para el GPS
 TinyGPSPlus gps;  
@@ -11,23 +12,24 @@ void setup() {
 Serial.begin(115200);
 Serial.println("Inicio de programa");
 
-GPS_SERIAL.begin(9600, SERIAL_8N1, 20, 21); // RX en GPIO3, TX en GPIO1 (TX no se usa)
+GPS_SERIAL.begin(9600, SERIAL_8N1, 20, 21); // RX en GPIO20, TX en GPIO21 (TX no se usa)
 }
 
 void loop() {
   while (GPS_SERIAL.available() > 0) {
     char c = GPS_SERIAL.read();
-    Serial.write(c); // Muestra la trama NMEA en el monitor serie
+    Serial.write(c); // Muestra la trama NMEA en el monitor serie (comentar si no se desea)
     gps.encode(c);   // Procesa el dato con TinyGPSPlus
   }
 
+
+
   // Ejemplo: mostrar latitud y longitud si hay datos válidos
-  if (gps.location.isUpdated()) {
+  if (gps.location.isValid()) {
     Serial.print("Latitud: ");
     Serial.println(gps.location.lat(), 6);
     Serial.print("Longitud: ");
     Serial.println(gps.location.lng(), 6);
-    
+    delay(2000); // Espera 2 segundos antes de la siguiente lectura
   }
 }
-
